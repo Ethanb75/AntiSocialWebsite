c3481b77e87a4cecfaf508f70562ea6a46ec401e
Ethan Bellora <Belloramail@gmail.com>
2017-09-05 15:52:42 -0400

    fixed build

--- gulpFile.js
+++ gulpFile.js
@@ -1,18 +1,23 @@
 'use strict'
 
 const gulp = require('gulp'),
-        sass = require('gulp-sass');
+        sass = require('gulp-sass'),
+        browserify = require('gulp-browserify');
 
 gulp.task('sass', function () {
   return gulp.src('./public/css/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/build/css'));
+});
+
+gulp.task('scripts', function() {
+    // Single entry point to browserify 
+    gulp.src('./public/js/index.js')
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('./public/build/js'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./public/css/*.scss', ['sass']);
-});
-
-
-
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['sass', 'scripts']);
