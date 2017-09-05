7c9c339c521cdc7a4f46fca4851374eaacdbc789
Ethan Bellora <Belloramail@gmail.com>
2017-09-05 11:06:19 -0400

    added a redirect from http -> https

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('*', function(req,res,next) {
+  if (PORT !== 3000) {
+    if(req.headers['x-forwarded-proto']!='https')
+      res.redirect('https://antisocial.to'+req.url);
+    else
+      next();
+  }
+});
+
 app.listen(PORT, () => {
   console.log('listening on port 3000')
 })
